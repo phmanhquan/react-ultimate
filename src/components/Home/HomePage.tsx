7b30cd3af2f14cc8707ef4604e31d03436addf0d
Quan Pham <Quan.Pham@vklink.vn>
2023-10-11 13:29:09 +0700

    design homepage

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+// import { Link } from "react-router-dom";
+import videoHome from "../../assets/video-home.mp4";
 
 const HomePage = () => {
-  return <div className="homepage-container">HomePage</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay loop muted>
+        <source src={videoHome} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
