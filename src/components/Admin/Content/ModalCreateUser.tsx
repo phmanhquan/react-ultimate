e6e1ebd0bc5724b0dadf8bc08794a856aad78682
Quan Pham <Quan.Pham@vklink.vn>
2023-10-13 11:50:54 +0700

    set state add new user

--- src/components/Admin/Content/ModalCreateUser.tsx
+++ src/components/Admin/Content/ModalCreateUser.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { FcPlus } from "react-icons/fc";
 
 const ModalCreateUser = () => {
   const [show, setShow] = useState(false);
@@ -9,7 +10,21 @@ const ModalCreateUser = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  // const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState<File>();
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(event.target.files[0]));
+      setImage(event.target.files[0]);
+    } else {
+      //
+    }
+  };
 
   return (
     <>
@@ -17,7 +32,12 @@ const ModalCreateUser = () => {
         Launch demo modal
       </Button>
 
-      <Modal show={show} onHide={handleClose} backdrop="static">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        className="modal-add-user"
+      >
         <Modal.Header closeButton>
           <Modal.Title>Add new user</Modal.Title>
         </Modal.Header>
@@ -25,28 +45,58 @@ const ModalCreateUser = () => {
           <Form className="row g-3">
             <div className="col-md-6">
               <label className="form-label">Username</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="col-md-4">
               <label className="form-label">Role</label>
-              <select className="form-select">
-                <option selected value="USER">
-                  USER
-                </option>
+              <select
+                className="form-select"
+                onChange={(event) => setRole(event.target.value)}
+              >
+                <option value="USER">USER</option>
                 <option value="ADMIN">ADMIN</option>
               </select>
             </div>
             <div className="col-md-12">
-              <label className="form-label">Image</label>
-              <input type="file" className="form-control" />
+              <label className="form-label label-upload" htmlFor="labelUpload">
+                <FcPlus /> Upload File Image
+              </label>
+              <input
+                type="file"
+                hidden
+                id="labelUpload"
+                onChange={(event) => handleUpload(event)}
+              />
+            </div>
+            <div className="col-md-12 img-preview">
+              {previewImage ? (
+                <img src={previewImage} alt="" />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </Form>
         </Modal.Body>
