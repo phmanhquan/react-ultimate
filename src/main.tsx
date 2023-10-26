41594cf146d47e7f60c9694955ea942ba6c4e914
Quan Pham <Quan.Pham@vklink.vn>
2023-10-26 15:47:30 +0700

    use redux persist to keep data when reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import store from "./redux/store.ts";
 import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./Layout.tsx";
 import "nprogress/nprogress.css";
+import { store, persistor } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <Layout />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <Layout />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>
 );
