d473e87b2c9de5504145b46d99b215a627f53009
Quan Pham <Quan.Pham@vklink.vn>
2023-11-07 17:10:08 +0700

    design dashboard

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -4,6 +4,8 @@ import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import { NavDropdown } from "react-bootstrap";
+import Language from "../Header/Language";
 
 const Admin = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -15,7 +17,16 @@ const Admin = () => {
       </div>
       <div className="admin-content">
         <div className="admin-header">
-          <FaBars onClick={() => setCollapsed(!collapsed)} />
+          <span onClick={() => setCollapsed(!collapsed)}>
+            <FaBars />
+          </span>
+          <div className="right-side">
+            <Language />
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log out</NavDropdown.Item>
+            </NavDropdown>
+          </div>
         </div>
         <div className="admin-main">
           <PerfectScrollbar>
--- src/components/Admin/Content/DashBoard.tsx
+++ src/components/Admin/Content/DashBoard.tsx
@@ -1,5 +1,79 @@
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import "./DashBoard.scss";
+
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+    },
+  ];
+  return (
+    <div className="dashboard-container">
+      <div className="title">Analytics DashBoard</div>
+      <div className="content">
+        <div className="content-left">
+          <div className="child">Total Users</div>
+          <div className="child">Total Quizzes</div>
+          <div className="child">Total Questions</div>
+          <div className="child">Total Answers</div>
+        </div>
+        <div className="content-right">
+          <ResponsiveContainer height="100%" width="100%">
+            <BarChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" fill="#8884d8" />
+              <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
